2884c3931d1eacd9370bb856cdd349d5c661b4d9
huichiaotsou <huichiao.tsou@gmail.com>
2021-12-29 13:08:35 +0800

    Force Process block for fix blocks heights cmd

--- cmd/fix/blocks/block.go
+++ cmd/fix/blocks/block.go
@@ -13,8 +13,8 @@ import (
 // blocksCmd returns a Cobra command that allows to fix missing blocks in database
 func blockCmd(parseConfig *parse.Config) *cobra.Command {
 	return &cobra.Command{
-		Use:   "refetch [block heights]",
-		Short: "Fix missing block and transaction in database for certain heights",
+		Use:   "heights [start height] [end height]",
+		Short: "Fix missing block and transaction in database for a range of heights",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			parseCtx, err := parse.GetParsingContext(parseConfig)
 			if err != nil {
@@ -24,14 +24,18 @@ func blockCmd(parseConfig *parse.Config) *cobra.Command {
 			workerCtx := parser.NewContext(parseCtx.EncodingConfig.Marshaler, nil, parseCtx.Node, parseCtx.Database, parseCtx.Logger, parseCtx.Modules)
 			worker := parser.NewWorker(0, workerCtx)
 
-			for _, k := range args {
-				k, err := strconv.ParseInt(k, 10, 64)
-				if err != nil {
-					return fmt.Errorf("error while converting block height %d to integer: %s", k, err)
-				}
+			startHeight, err := strconv.ParseInt(args[0], 10, 64)
+			if err != nil {
+				return fmt.Errorf("error while converting start height %s to integer: %s", args[0], err)
+			}
+			endHeight, err := strconv.ParseInt(args[1], 10, 64)
+			if err != nil {
+				return fmt.Errorf("error while converting end height %s to integer: %s", args[1], err)
+			}
 
+			for k := startHeight; k <= endHeight; k++ {
 				fmt.Printf("Refetching missing blocks and transactions for height %v ... \n", k)
-				err = worker.Process(k)
+				err = worker.ForceProcess(k)
 				if err != nil {
 					return fmt.Errorf("error while re-fetching block %d: %s", k, err)
 				}
